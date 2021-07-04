b89e9ce458a7b91a0fd40a458352d01ce73534fb
Sweetyxz <2364539601@qq.com>
2021-07-04 21:08:24 +0800

    Create chapter_8.py

---
+++ python_test/chapter_8.py
@@ -0,0 +1,8 @@
+#8-1 8-2 
+def display_message():
+	print("i learn the def")
+display_message()
+
+def favorite_book(title):
+	print(f"one of my favorite book is {title.title()}")
+favorite_book('harry potter')
