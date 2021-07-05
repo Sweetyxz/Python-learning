5041795101c1b351e886ad179c67c2e27aaaa0d4
Sweetyxz <2364539601@qq.com>
2021-07-05 22:26:58 +0800

    Update chapter_8.py

--- python_test/chapter_8.py
+++ python_test/chapter_8.py
@@ -1,4 +1,5 @@
 #8-1 8-2 
+'''
 def display_message():
 	print("i learn the def")
 display_message()
@@ -6,3 +7,92 @@ display_message()
 def favorite_book(title):
 	print(f"one of my favorite book is {title.title()}")
 favorite_book('harry potter')
+'''
+
+'''
+def name_book(name, book):
+	print(f"{name} like {book}")
+name_book(name = 'zyx', book = 'harry potter')
+'''
+
+'''
+def name_book(name = 'zyx', book = 'hp'): #默认值
+	print(f"{name} like {book}")
+name_book()
+'''
+
+# 8-3 8-4 8-5
+'''
+def make_shirt(size, patten):
+	print(f"the size is {size},the patten is {patten} ")
+make_shirt('M', 'I LOVE CHINA')
+make_shirt(patten = 'i LOVE my country', size = 'L')
+'''
+
+'''
+def make_shirt(size, patten = 'i love python'):
+	print(f"the size is {size},the patten is {patten} ")
+make_shirt('m')
+make_shirt('L')
+make_shirt('s', 'i love php')
+
+def describe_city(city, country = 'china'):
+	print(f"{city} is in {country}")
+describe_city('beijing')
+describe_city('zhengzhou')
+describe_city('new york', 'Amarica')
+'''
+
+#8-6  8-7 8-8
+'''
+def city_country(city, country):
+	mes = f"{city}, {country}"
+	return mes
+message = city_country("beijing", "china")
+print(message)
+message = city_country("zhengzhou", "china")
+print(message)
+message = city_country("hangzhou", "china")
+print(message)
+
+def make_album(singer, album_name, num = None):
+	album = {'singer': singer, 'album_name': album_name}
+	if num:
+		album['num'] = num
+	return album
+zyx = make_album('zyx', 'hi')
+lmy = make_album('lmy', 'hello', num = 3)
+print(zyx)
+print(lmy)
+'''
+
+'''
+def make_album(singer, album_name, num = None):
+	album = {'singer': singer, 'album_name': album_name}
+	if num:
+		album['num'] = num
+	return album
+while True:
+	print("press q to quit")
+	name = input("name:")
+	album_name = input("album_name:")
+	if name == 'q' or album_name == 'q':
+		break
+	print(make_album(name, album_name))
+'''
+
+# 8-9 8-10 8-11
+mes = ["hi", "hello", "hh"]
+sent_messages = []
+def show_message(mes):
+	for m in mes:
+		print(m)
+
+def send_messages(mes):
+	while mes:
+		x = mes.pop()
+		sent_messages.append(x)
+
+send_messages(mes[:])
+print(mes)
+show_message(sent_messages)
