e69bb873a1d19a3f6810668fcac2bfee2d7c2ce0
Sweetyxz <2364539601@qq.com>
2021-07-12 22:09:47 +0800

    新增飞船发出子弹功能

--- python_test/Alien_Invasion/alien_invasion.py
+++ python_test/Alien_Invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from setting import settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -15,17 +16,22 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
         self.ship = Ship(self)  # self指向当前实例
 
-    def check_keydown_event(self, event):  # 按下键盘检查
+        self.bullets = pygame.sprite.Group()
+
+    def check_keydown_event(self, event):  # 飞船的按下键盘检查
         if event.key == pygame.K_RIGHT:
             self.ship.move_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.move_left = True
+        elif event.key == pygame.K_SPACE:
+            self.fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
-    def check_keyup_event(self, event):  # 松开键盘检查
+    def check_keyup_event(self, event):  # 飞船的松开键盘检查
         if event.key == pygame.K_RIGHT:
             self.ship.move_right = False
         elif event.key == pygame.K_LEFT:
@@ -40,15 +46,30 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self.check_keyup_event(event)
 
+    def fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_number_allowed:
+            bullet = Bullet(self)  # 实例化bullet元素
+            self.bullets.add(bullet)  # 添加到编组中
+
     def update_screen(self):  # 屏幕更新
         self.screen.fill(self.settings.bg_color)
         self.ship.biteme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()  # 绘制的屏幕可见
 
-    def run_game(self):
+    def update_buttle(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():  #使用编组的副本
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def run_game(self):  # 循环判断事件更新屏幕
         while True:
             self.check_event()
             self.ship.updata()
+            self.update_buttle()
             self.update_screen()
 
 
--- python_test/Alien_Invasion/setting.py
+++ python_test/Alien_Invasion/setting.py
@@ -4,4 +4,11 @@ class settings:
         self.screen_width = 600
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+
         self.ship_speed = 1.5
+
+        self.bullet_color = (60, 60, 60)
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_number_allowed = 3
